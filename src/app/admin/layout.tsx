2de65f8b1ee537007f36035f6da8e118db989cd6
franco-barros <francobarrosdaniel@gmail.com>
2025-06-25 19:53:36 -0300

    Bug fixed

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,45 +1,6 @@
-"use client";
-
-import { Sidebar } from "@/components/sidebar";
-import styles from "../../styles/admin/AdminPage.module.css";
-import { ArrowLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
-
-type ContentType = "article" | "testimonial" | "tip";
-
-interface AdminLayoutProps {
-  children: React.ReactNode;
-  type: ContentType;
-  setType: (type: ContentType) => void;
-  setMode: (mode: "create" | "list") => void;
-}
-
+// app/admin/layout.tsx
 export default function AdminLayout({
   children,
-  type,
-  setType,
-  setMode,
-}: Readonly<AdminLayoutProps>) {
-  const router = useRouter();
-
-  return (
-    <div className={styles.adminContainer}>
-      <button onClick={() => router.push("/")} className={styles.backButton}>
-        <ArrowLeft className={styles.backIcon} />
-        Volver al inicio
-      </button>
-
-      <div className={styles.adminLayout}>
-        <Sidebar
-          selected={type}
-          onSelect={(newType: ContentType) => {
-            setType(newType);
-            setMode("create");
-          }}
-        />
-
-        <main className={styles.formWrapper}>{children}</main>
-      </div>
-    </div>
-  );
+}: Readonly<{ children: React.ReactNode }>) {
+  return <>{children}</>;
 }
